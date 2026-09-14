fbe5a7cbbd483dd9e74d4524daec5b3d67129e12
Jovana <jovanakostojchinoska@gmail.com>
2026-09-14 23:56:13 +0200

    Add invitation metadata

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,17 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "🎂 Прв родендеден",
-  description: "Со задоволство Ве покануваме да го прославиме првиот роденден.",
+  title: "🎂 Прв роденден",
+  description:
+    "Со особена радост Ве покануваме на прославата на првиот роденден.",
 
   openGraph: {
     title: "🎂 Прв роденден",
     description:
-      "Со задоволство Ве покануваме да го прославиме првиот роденден.",
-    images: [
-      {
-        url: "/slika.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Роденденска покана",
-      },
-    ],
+      "Со особена радост Ве покануваме на прославата на првиот роденден.",
+    url: "https://rodenden.vercel.app/",
+    siteName: "Прва роденденска прослава",
     type: "website",
-  },
-
-  twitter: {
-    card: "summary_large_image",
-    title: "🎂 Прв роденден",
-    description:
-      "Со задоволство Ве покануваме да го прославиме првиот роденден.",
-    images: ["/slika.jpg"],
   },
 };
 
